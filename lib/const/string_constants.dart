d0c10acd774a7c6a1ea5bb73f69edebfc79af583
dev-6768 <learnandgrowonefiveoneone@gmail.com>
2024-09-26 22:18:31 +0530

    final changes

--- lib/const/string_constants.dart
+++ lib/const/string_constants.dart
@@ -1,4 +1,4 @@
 class StringConstants {
-  static const apiKey = "bf70f617568442948b81cb60d181df19";
+  static const apiKey = "INSERT_API_KEY_HERE"; //SAMPLE KEY : bf70f617568442948b81cb60d181df19
   static const developmentBaseUrl = "https://newsapi.org/";
 }
